b17c186a6ad297ac1be5f4a3cf31d7281e384577
Mathieu Delehaye <mathieu.delehaye@gmail.com>
2020-10-09 12:55:03 +0200

    Connection to MongoDB with Mongoose added.

--- app.js
+++ app.js
@@ -3,11 +3,9 @@
 const express = require('express')
 const bodyParser = require("body-parser")
 const date = require(__dirname + "/date.js")
+const mongoose = require("mongoose")
 
 const app = express()
-
-const items = ["Buy Food", "Cook Food", "Eat Food"]
-const workItems = []
 
 app.set('view engine', 'ejs')
 
@@ -16,13 +14,53 @@ app.use(bodyParser.urlencoded({
 }))
 app.use(express.static("public"))
 
+mongoose.connect("mongodb://localhost:27017/todolistDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+
+const itemSchema = {
+  name: String
+}
+
+const Item = mongoose.model("Item", itemSchema)
+
+const item1 = new Item({
+  name: "Welcome to your todolist!"
+})
+
+const item2 = new Item({
+  name: "Hit the + button to add a new item."
+})
+
+const item3 = new Item({
+  name: "<-- Hit this to delete an item."
+})
+
+const defaultItems = [item1, item2, item3]
+
 app.get("/", function(req, res) {
 
-  const day = date.getDate()
+  Item.find({}, function(err, foundItems) {
 
-  res.render("list", {
-    listTitle: day,
-    newListitems: items
+    if (foundItems.length === 0) {
+
+      Item.insertMany(defaultItems, function(err, docs) {
+        if (err) {
+          console.log(err)
+        } else {
+          console.log("Successfully saved all the items to DB.")
+        }
+      })
+      res.redirect("/") // reload root route
+    } else {
+
+      res.render("list", {
+        listTitle: "Today",
+        newListitems: foundItems
+
+      })
+    }
   })
 })
 
